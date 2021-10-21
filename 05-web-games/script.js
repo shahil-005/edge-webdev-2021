05561e3305d5f7656980c01151af8fe6a433b4ee
Arnav Gupta <arnav.gupta@scaler.com>
2021-10-21 22:01:40 +0530

    moving the enemy block
    Signed-off-by: Arnav Gupta <arnav.gupta@scaler.com>

--- 05-web-games/script.js
+++ 05-web-games/script.js
@@ -1,14 +1,53 @@
 let gamebox = document.getElementById('gamebox');
 let context = gamebox.getContext('2d');
 
-context.fillStyle = 'brown';
+let enemy = {
+  color: 'red',
+  x: 100,
+  y: 0,
+  h: 50,
+  w: 50,
+  vx: 0,
+  vy: 1
+}
 
-let posX = 0;
-let posY = 0;
+let player = {
+  color: 'blue',
+  x: 0,
+  y: 175,
+  h: 50,
+  w: 50,
+  vx: 1,
+  vy: 0
+}
 
-setInterval(function () {
-  posX += 10;
-  posY += 10;
+function drawBox(box) {
+  context.fillStyle = box.color;
+  context.fillRect(box.x, box.y, box.w, box.h);
+}
+
+function updateGameState() {
+  enemy.y += enemy.vy;
+  if (enemy.y + enemy.h > gamebox.height) {
+    enemy.vy = -1;
+  } else if (enemy.y < 0) {
+    enemy.vy = 1;
+  }
+  
+}
+
+function updateGame() {
+  // update game state 
+  updateGameState();
+
+  // clear the canvas
   context.clearRect(0, 0, gamebox.width, gamebox.height);
-  context.fillRect(posX, posY, 20, 20);
-}, 2000)
+  // draw the player
+  drawBox(player);
+  // draw the enemy
+  drawBox(enemy);
+
+  window.requestAnimationFrame(updateGame);
+}
+
+updateGame();
